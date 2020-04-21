305aebd5cf1c734e97e545211c629fd5ccd092f1
NamBui98 <bvnam98@gmail.com>
2020-04-22 00:27:21 +0700

    Updates

---
+++ precache-manifest.00ce400563d8a0323ff4716937606c56.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b59906f2c388af0dcba3d172be02c724",
+    "url": "/convertToReactjs/index.html"
+  },
+  {
+    "revision": "6e92c04761b318b513b9",
+    "url": "/convertToReactjs/static/css/2.a43b59a1.chunk.css"
+  },
+  {
+    "revision": "adbaba258e361595223e",
+    "url": "/convertToReactjs/static/css/main.da01bb18.chunk.css"
+  },
+  {
+    "revision": "6e92c04761b318b513b9",
+    "url": "/convertToReactjs/static/js/2.aace603b.chunk.js"
+  },
+  {
+    "revision": "413b595604c5d3d1d7baf18f18130db2",
+    "url": "/convertToReactjs/static/js/2.aace603b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adbaba258e361595223e",
+    "url": "/convertToReactjs/static/js/main.48b239f1.chunk.js"
+  },
+  {
+    "revision": "54a370843d11469ca2f6",
+    "url": "/convertToReactjs/static/js/runtime-main.aec1415d.js"
+  }
+]);
